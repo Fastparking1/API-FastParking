9627acefa2a424b923059b72ac20144f6a8513e6
Fastparking1 <142273414+Fastparking1@users.noreply.github.com>
2023-11-01 11:23:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const sequelize = new Sequelize({
 });
 
 // Criação de tabela
+// Atualização do modelo Cliente
 const Cliente = sequelize.define('Cliente', {
   cli_id: {
     type: DataTypes.INTEGER,
@@ -22,88 +23,67 @@ const Cliente = sequelize.define('Cliente', {
     autoIncrement: true,
   },
   cli_nome: {
-    type: DataTypes.STRING(60),
-    allowNull: false,
+    type: DataTypes.STRING,
   },
-  cli_senha: {
-    type: DataTypes.STRING(60),
-    allowNull: false,
-  },
-  cli_email: {
-    type: DataTypes.STRING(60),
-    unique: true,
-  },
-  cli_telefone: {
-    type: DataTypes.STRING(12),
-    allowNull: false,
-  },
-  cli_endereco: {
-    type: DataTypes.STRING(200),
-    allowNull: false,
-  },
-  cli_data_nasc: {
-    type: DataTypes.DATE,
+  cli_sobrenome: {
+    type: DataTypes.STRING,
   },
   cli_sexo: {
-    type: DataTypes.ENUM('M', 'F'),
+    type: DataTypes.STRING,
   },
   cli_cpf: {
-    type: DataTypes.STRING(11),
+    type: DataTypes.STRING,
+  },
+  cli_nascimento: {
+    type: DataTypes.STRING,
+  },
+  cli_senha: {
+    type: DataTypes.STRING,
+  },
+  cli_confirmacao: {
+    type: DataTypes.STRING,
   },
   cli_status: {
-    type: DataTypes.BOOLEAN,
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
   },
 });
 
+// Atualização do modelo Empresa
 const Empresa = sequelize.define('Empresa', {
   emp_id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
   },
-  emp_cnpj: {
-    type: DataTypes.STRING(14),
-    allowNull: false,
-  },
-  emp_situacao_cadastral: {
-    type: DataTypes.ENUM(
-      'ativa',
-      'inativa',
-      'suspensa',
-      'baixada',
-      'inapta',
-      'atividade_rural',
-      'MEI',
-      'estrangeira',
-      'concordata',
-      'recuperacao_judicial'
-    ),
-    allowNull: false,
-  },
-  emp_data_abertura: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
   emp_nome_empresarial: {
-    type: DataTypes.STRING(100),
+    type: DataTypes.STRING,
   },
-  emp_atividades_economicas: {
-    type: DataTypes.STRING(255),
+  emp_cnpj: {
+    type: DataTypes.STRING,
   },
-  emp_natureza_juridica: {
-    type: DataTypes.STRING(255),
+  emp_cep: {
+    type: DataTypes.STRING,
   },
-  emp_endereco_contato: {
-    type: DataTypes.STRING(255),
+  emp_endereco: {
+    type: DataTypes.STRING,
   },
-  emp_capital: {
-    type: DataTypes.SMALLINT,
+  emp_bairro: {
+    type: DataTypes.STRING,
+  },
+  emp_numero: {
+    type: DataTypes.STRING,
+  },
+  emp_complemento: {
+    type: DataTypes.STRING,
   },
   emp_status: {
-    type: DataTypes.BOOLEAN,
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
   },
 });
 
+// Atualização do modelo Estacionamento
 const Estacionamento = sequelize.define('Estacionamento', {
   est_id: {
     type: DataTypes.INTEGER,
@@ -112,24 +92,28 @@ const Estacionamento = sequelize.define('Estacionamento', {
   },
   est_qnt_vagas: {
     type: DataTypes.INTEGER,
-    allowNull: false,
+    defaultValue: 30,
   },
   est_endereco: {
-    type: DataTypes.STRING(200),
-    allowNull: false,
+    type: DataTypes.STRING,
   },
   est_status: {
-    type: DataTypes.BOOLEAN,
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
   },
 });
 
-const ClienteEmpresa = sequelize.define('ClienteEmpresa', {}, { timestamps: false });
+// Tabela intermediária para o relacionamento entre Cliente e Empresa
+const ClienteEmpresa = sequelize.define('ClienteEmpresa', {});
 
-const EmpresaEstacionamento = sequelize.define('EmpresaEstacionamento', {}, { timestamps: false });
-
+// Defina a relação muitos para muitos entre Cliente e Empresa
 Cliente.belongsToMany(Empresa, { through: ClienteEmpresa });
 Empresa.belongsToMany(Cliente, { through: ClienteEmpresa });
 
+// Tabela intermediária para o relacionamento entre Empresa e Estacionamento
+const EmpresaEstacionamento = sequelize.define('EmpresaEstacionamento', {});
+
+// Defina a relação muitos para muitos entre Empresa e Estacionamento
 Empresa.belongsToMany(Estacionamento, { through: EmpresaEstacionamento });
 Estacionamento.belongsToMany(Empresa, { through: EmpresaEstacionamento });
 
@@ -143,91 +127,28 @@ sequelize.sync({ force: true })
 
   app.use(express.json());
 
-  app.post('/clientes', async (req, res) => {
-    const {
-      cli_nome,
-      cli_senha,
-      cli_email,
-      cli_telefone,
-      cli_endereco,
-      cli_data_nasc,
-      cli_sexo,
-      cli_cpf,
-      cli_status,
-    } = req.body;
-  
-    try {
-      const cliente = await Cliente.create({
-        cli_nome,
-        cli_senha,
-        cli_email,
-        cli_telefone,
-        cli_endereco,
-        cli_data_nasc,
-        cli_sexo,
-        cli_cpf,
-        cli_status,
-      });
-      res.json(cliente);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Erro ao inserir cliente');
-    }
-  });
-  
   app.post('/cadastro', async (req, res) => {
-    const {
-      emp_cnpj,
-      emp_situacao_cadastral,
-      emp_data_abertura,
-      emp_nome_empresarial,
-      emp_atividades_economicas,
-      emp_natureza_juridica,
-      emp_endereco_contato,
-      emp_capital,
-      emp_status,
-    } = req.body;
+    const { entidade } = req.body;
   
     try {
-      const empresa = await Empresa.create({
-        emp_cnpj,
-        emp_situacao_cadastral,
-        emp_data_abertura,
-        emp_nome_empresarial,
-        emp_atividades_economicas,
-        emp_natureza_juridica,
-        emp_endereco_contato,
-        emp_capital,
-        emp_status,
-      });
-      res.json(empresa);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Erro ao inserir empresa');
-    }
-  });
-
-  app.get('/login', async (req, res) => {
-    const { cli_email, cli_senha } = req.query;
-  
-    try {
-      const cliente = await Cliente.findOne({
-        where: {
-          cli_email,
-          cli_senha,
-        },
-      });
-  
-      if (cliente) {
-        res.json({ message: 'Login bem-sucedido', cliente });
+      if (entidade === 'cliente') {
+        const cliente = await Cliente.create(req.body);
+        res.json(cliente);
+      } else if (entidade === 'empresa') {
+        const empresa = await Empresa.create(req.body);
+        res.json(empresa);
+      } else if (entidade === 'estacionamento') {
+        const estacionamento = await Estacionamento.create(req.body);
+        res.json(estacionamento);
       } else {
-        res.status(401).json({ message: 'Credenciais inválidas' });
+        res.status(400).json({ message: 'Entidade inválida' });
       }
     } catch (err) {
       console.error(err);
-      res.status(500).send('Erro ao autenticar cliente');
+      res.status(500).send('Erro ao cadastrar entidade');
     }
   });
+  
   
   
 
