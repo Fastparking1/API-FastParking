61d17159007016fe1f40cdb8348f9f2ddc8e29f5
Fastparking1 <142273414+Fastparking1@users.noreply.github.com>
2023-11-01 16:35:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
+const cors = require('cors');
 const config = require('./config/config.json')[process.env.NODE_ENV];
 
 const app = express();
 const port = 3000;
+
+const corsOptions = {
+  origin: 'https://fastparking-9b5f7.firebaseapp.com', // Substitua pelo domínio do seu aplicativo Angular
+  optionsSuccessStatus: 200, // Opcional
+};
+
+app.use(cors(corsOptions));
 
 //Conexão com do sequelize
 const sequelize = new Sequelize({
@@ -14,142 +22,142 @@ const sequelize = new Sequelize({
   password: config.password,
 });
 
-// Criação de tabela
-// Atualização do modelo Cliente
+
 const Cliente = sequelize.define('Cliente', {
-  cli_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  cli_nome: {
+  sobrenome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sexo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sexoOutro: {
     type: DataTypes.STRING,
   },
-  cli_sobrenome: {
+  nasci: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
-  cli_sexo: {
+  email: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
-  cli_cpf: {
+  cpf: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
-  cli_nascimento: {
+  cnpj: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
-  cli_senha: {
+  cep: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
-  cli_confirmacao: {
+  endereco: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
-  cli_status: {
-    type: DataTypes.INTEGER,
-    defaultValue: 1,
+  bairro: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  numero: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  complemento: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  confirmacao: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
 });
 
-// Atualização do modelo Empresa
-const Empresa = sequelize.define('Empresa', {
-  emp_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  emp_nome_empresarial: {
-    type: DataTypes.STRING,
-  },
-  emp_cnpj: {
-    type: DataTypes.STRING,
-  },
-  emp_cep: {
-    type: DataTypes.STRING,
-  },
-  emp_endereco: {
-    type: DataTypes.STRING,
-  },
-  emp_bairro: {
-    type: DataTypes.STRING,
-  },
-  emp_numero: {
-    type: DataTypes.STRING,
-  },
-  emp_complemento: {
-    type: DataTypes.STRING,
-  },
-  emp_status: {
-    type: DataTypes.INTEGER,
-    defaultValue: 1,
-  },
-});
-
-// Atualização do modelo Estacionamento
-const Estacionamento = sequelize.define('Estacionamento', {
-  est_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  est_qnt_vagas: {
-    type: DataTypes.INTEGER,
-    defaultValue: 30,
-  },
-  est_endereco: {
-    type: DataTypes.STRING,
-  },
-  est_status: {
-    type: DataTypes.INTEGER,
-    defaultValue: 1,
-  },
-});
-
-// Tabela intermediária para o relacionamento entre Cliente e Empresa
-const ClienteEmpresa = sequelize.define('ClienteEmpresa', {});
-
-// Defina a relação muitos para muitos entre Cliente e Empresa
-Cliente.belongsToMany(Empresa, { through: ClienteEmpresa });
-Empresa.belongsToMany(Cliente, { through: ClienteEmpresa });
-
-// Tabela intermediária para o relacionamento entre Empresa e Estacionamento
-const EmpresaEstacionamento = sequelize.define('EmpresaEstacionamento', {});
-
-// Defina a relação muitos para muitos entre Empresa e Estacionamento
-Empresa.belongsToMany(Estacionamento, { through: EmpresaEstacionamento });
-Estacionamento.belongsToMany(Empresa, { through: EmpresaEstacionamento });
-
-sequelize.sync({ force: true })
+// Synchronize the model with the database
+sequelize.sync()
   .then(() => {
-    console.log('Tabelas criadas com sucesso.');
+    console.log('Tabela "Cliente" criada com sucesso.');
   })
   .catch((error) => {
-    console.error('Erro ao criar tabelas:', error);
+    console.error('Erro ao criar a tabela "Cliente":', error);
   });
 
   app.use(express.json());
 
-  app.post('/cadastro', async (req, res) => {
-    const { entidade } = req.body;
+  app.post('/cadastrar-cliente', async (req, res) => {
+    const { nome, sobrenome, sexo, sexoOutro, nasci, email, cpf, cnpj, cep, endereco, bairro, numero, complemento, senha, confirmacao  } = req.body;
   
     try {
-      if (entidade === 'cliente') {
-        const cliente = await Cliente.create(req.body);
-        res.json(cliente);
-      } else if (entidade === 'empresa') {
-        const empresa = await Empresa.create(req.body);
-        res.json(empresa);
-      } else if (entidade === 'estacionamento') {
-        const estacionamento = await Estacionamento.create(req.body);
-        res.json(estacionamento);
-      } else {
-        res.status(400).json({ message: 'Entidade inválida' });
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Erro ao cadastrar entidade');
+      // Crie um novo cliente no banco de dados
+      const cliente = await Cliente.create({
+        nome,
+        sobrenome,
+        sexo,
+        sexoOutro, 
+        nasci, 
+        email, 
+        cpf, 
+        cnpj, 
+        cep, 
+        endereco, 
+        bairro, 
+        numero, 
+        complemento, 
+        senha, 
+        confirmacao
+      });
+  
+      // Envie uma resposta de sucesso com o cliente cadastrado
+      res.json(cliente);
+    } catch (error) {
+      console.error('Erro ao cadastrar cliente:', error);
+      res.status(500).json({ message: 'Erro ao cadastrar cliente' });
     }
   });
   
+  app.post('/login', async (req, res) => {
+    const { email, senha } = req.body;
   
+    try {
+      // Aqui você deve adicionar a lógica de autenticação
+      // Consultar o banco de dados para verificar se o email e senha correspondem a um usuário válido
+      // Vou fornecer um exemplo simples usando o modelo de Cliente, mas você deve adaptar isso à sua autenticação real
+  
+      const cliente = await Cliente.findOne({ where: { email, senha } });
+  
+      if (cliente) {
+        // Autenticação bem-sucedida, você pode gerar um token de autenticação ou definir uma sessão
+        res.status(200).json({ message: 'Login bem-sucedido' });
+      } else {
+        res.status(401).json({ message: 'Credenciais inválidas' });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erro ao fazer login');
+    }
+  });
+
+  app.get('/listar-clientes', async (req, res) => {
+    try {
+      const clientes = await Cliente.findAll();
+      res.json(clientes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erro ao listar clientes');
+    }
+  });
   
 
 app.listen(port, () => {
